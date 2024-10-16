ae3c46b87042fedc6d4fe1da27cf38e00d2234aa
Antonia Elsen <antoniaelsen@gmail.com>
2024-10-15 22:57:19 -0700

    chore: lint

--- include/science/libndtp/ndtp.h
+++ include/science/libndtp/ndtp.h
@@ -7,9 +7,6 @@
 #include "science/libndtp/utils.h"
 #include "science/libndtp/synapse/api/datatype.pb.h"
 
-// #define htonll(x) ((1 == htonl(1)) ? (x) : ((uint64_t)htonl((x) & 0xFFFFFFFF) << 32) | htonl((x) >> 32))
-// #define ntohll(x) ((1 == ntohl(1)) ? (x) : ((uint64_t)ntohl((x) & 0xFFFFFFFF) << 32) | ntohl((x) >> 32))
-
 namespace science::libndtp {
 
 static constexpr uint8_t NDTP_VERSION = 0x01;
