de8d40f98f5b9f6b7d9448bb961552e5955f55a6
Antonia Elsen <antoniaelsen@gmail.com>
2024-10-21 17:00:33 -0700

    fix: define long long hton ntoh for linux

--- include/science/libndtp/ndtp.h
+++ include/science/libndtp/ndtp.h
@@ -7,6 +7,11 @@
 #include "science/libndtp/utils.h"
 #include "science/libndtp/synapse/api/datatype.pb.h"
 
+#ifdef __linux__
+#define htonll(x) ((1 == htonl(1)) ? (x) : ((uint64_t)htonl((x) & 0xFFFFFFFF) << 32) | htonl((x) >> 32))
+#define ntohll(x) ((1 == ntohl(1)) ? (x) : ((uint64_t)ntohl((x) & 0xFFFFFFFF) << 32) | ntohl((x) >> 32))
+#endif
+
 namespace science::libndtp {
 
 static constexpr uint8_t NDTP_VERSION = 0x01;
