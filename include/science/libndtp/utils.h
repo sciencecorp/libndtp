30926b275316329e5ab963ee6f65e48ef78ac25a
Sage <sage@caesium.dev>
2024-10-24 15:16:25 -0700

    Remove exceptions from unsigned packing in to_bytes()

--- include/science/libndtp/utils.h
+++ include/science/libndtp/utils.h
@@ -74,16 +74,7 @@ std::pair<ByteArray, BitOffset> to_bytes(
       if (val < 0) {
         val = (1 << bit_width) + val;
       }
-    } else {
-      if (val < 0) {
-        throw std::invalid_argument("unsigned packing specified, but value is negative");
-      }
-      if (val >= (1 << bit_width)) {
-        throw std::invalid_argument(
-          "unsigned value " + std::to_string(val) + " doesn't fit in " + std::to_string(bit_width) + " bits"
-        );
-      }
-    }
+    } 
 
     int remaining_bits = bit_width;
     while (remaining_bits > 0) {
